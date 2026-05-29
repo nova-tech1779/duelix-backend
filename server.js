15eef3cd63d1fb438749a6cd4d714ff2b0f1d861
nova-tech1779 <buddybounce58@gmail.com>
2026-05-29 13:58:44 +0000

    10

--- server.js
+++ server.js
@@ -2009,7 +2009,7 @@ app.post("/users/create-profile", verifyToken, async (req, res) => {
         displayName:           name,
         phone,
         email:                 email != null ? email : "",
-        coins:                 20,
+        coins:                 10,
         wins:                  0,
         losses:                0,
         draws:                 0,
